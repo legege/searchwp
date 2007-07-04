60f8c0e2bdff91b42276ae6755df3211ad1c5d24
Georges-Etienne Legendre <legege@users.noreply.github.com>
2007-07-04 17:49:49 +0000

    * ANT Build script refactoring

--- src/defaults/preferences/searchwp.js
+++ src/defaults/preferences/searchwp.js
@@ -1,4 +1,4 @@
-pref("extensions.{3e270ac3-8936-43fb-ad20-b4685172a83d}.description", "chrome://searchwp/locale/searchwp.properties");
+pref("extensions.@ID@.description", "chrome://@NAME@/locale/searchwp.properties");
 pref("extensions.searchwp.highlighter-count", 4);
 pref("extensions.searchwp.highlight-state", false);
 pref("extensions.searchwp.first-launch", true);
