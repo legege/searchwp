6a8782926af96af28735bd3d0fffbe694269678c
legege <legege@76b47386-e832-0410-97a5-4fc110babfa0>
2008-03-01 02:04:11 +0000

    * SearchWP 2.0

---
+++ tags/searchwp-2.0/src/chrome/content/options/optionsDialog.js
@@ -0,0 +1,37 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is SearchWP.
+ *
+ * The Initial Developer of the Original Code is 
+ *  Georges-Etienne Legendre <legege@legege.com> <http://legege.com>.
+ * Portions created by the Initial Developer are Copyright (C) 2004-2008.
+ * All Rights Reserved.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+searchwp.options.OptionsDialog = new function() {
+  this.onLoad = function() {
+    document.getElementById("highlightMinlength").selectedIndex = searchwp.Preferences.highlightMinLength - 1;
+  }
+
+  this.onAccept = function() {
+    // Highlighting options
+    var highlightMinLength = document.getElementById("highlightMinlength").selectedIndex + 1;
+    if (highlightMinLength > 0) {
+      searchwp.Preferences.highlightMinLength = highlightMinLength;
+    }
+
+    return true;
+  }
+}
