949a0d4b419e9f041a8ceb0a1dd0232c07075ebb
legege <legege@76b47386-e832-0410-97a5-4fc110babfa0>
2008-08-20 03:36:27 +0000

    * Updating locales (script adjustment)

--- trunk/updateLocales.sh
+++ trunk/updateLocales.sh
@@ -8,5 +8,6 @@ for locale in `ls $LOCALES_SRC`; do
   echo "Copying file for $locale..."
   mkdir -p $LOCALES_DEST/$locale/
   cp -r $LOCALES_SRC/$locale/* $LOCALES_DEST/$locale/
-  svn add  $LOCALES_DEST/$locale/* 2> /dev/null
+  svn add $LOCALES_DEST/$locale 2> /dev/null
+  svn add $LOCALES_DEST/$locale/* 2> /dev/null
 done
