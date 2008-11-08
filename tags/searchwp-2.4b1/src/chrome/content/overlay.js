657c44602bd7e03d96d4e7de9676c6ce4c0005fe
legege <legege@76b47386-e832-0410-97a5-4fc110babfa0>
2008-11-08 15:39:55 +0000

    SearchWP 2.4b1

---
+++ tags/searchwp-2.4b1/src/chrome/content/overlay.js
@@ -0,0 +1,218 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is SearchWP.
+ *
+ * The Initial Developer of the Original Code is 
+ *  Georges-Etienne Legendre <legege@legege.com> <http://legege.com>.
+ * Portions created by the Initial Developer are Copyright (C) 2004-2008.
+ * All Rights Reserved.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+gSearchWP.Overlay = new function() {
+  var self = this;
+  var _stringBundle = null;
+  var _oldCustomizeDone;
+
+  /**
+   * Initializes this extension.
+   * @param aEvent The load event.
+   */
+  this.onLoad = function(aEvent) {
+    if (aEvent.target != document || !window.getBrowser) {
+      return;
+    }
+
+    _stringBundle = document.getElementById("bundle-searchwp");
+
+    this.preferencesObserver.register();
+    window.getBrowser().addProgressListener(this.progressListener,
+                                            Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
+
+    /* XXXLegege: Unfortunately, there is no event on the termisation of the
+       toolbox customization. All EventListeners of the searchbar are lost when
+       the user open the toolbox customizer. So, we have to reset/set those.
+       Also, we cannot replace the customizeDone function without a setTimeout. */
+    setTimeout(function() {
+      var toolbox = document.getElementById("navigator-toolbox");
+      if (toolbox.customizeDone != gSearchWP.Overlay.onCustomizeDone) {
+        _oldCustomizeDone = toolbox.customizeDone;
+        toolbox.customizeDone = gSearchWP.Overlay.onCustomizeDone;
+      }
+    }, 0);
+
+    if (gSearchWP.Preferences.firstLaunch && !gSearchWP.Highlighting.exist()) {
+      setTimeout(function() {
+        gSearchWP.Overlay.firstLaunch();
+      }, 50);
+    }
+
+    // Add platform CSS selection
+    var win = document.getElementById("main-window");
+    win.setAttribute("searchwp-moz-platform", navigator.platform);
+
+    gSearchWP.Highlighting.init();
+
+    addEventListener("unload", function(aEvent) { gSearchWP.Overlay.onUnload(aEvent); }, false);
+  }
+
+  /**
+   * Uninitializes this extension.
+   * @param aEvent The unload event.
+   */
+  this.onUnload = function(aEvent) {
+    this.preferencesObserver.unregister();
+    window.getBrowser().removeProgressListener(this.progressListener);
+  }
+
+  /**
+   * @return the searchbox
+   */
+  this.__defineGetter__("searchbox", function() {
+    var searchbar = document.getElementById("searchbar");
+    if (searchbar) {
+      return document.getAnonymousElementByAttribute(searchbar, "anonid", "searchbar-textbox");
+    }
+    return null;
+  });
+
+  /**
+   * firstLaunch
+   */
+  this.firstLaunch = function() {
+    // Adding the highlight button to the toolbar
+    // http://developer.mozilla.org/en/docs/Code_snippets:Toolbar#Adding_button_by_default
+    try {
+      var nav = document.getElementById("nav-bar");
+      var curSet = nav.currentSet;
+      if (curSet.indexOf("searchwp-highlight-container") == -1) {
+        var set;
+        // Place the button before the searchbox
+        if (curSet.indexOf("search-container") != -1) {
+          set = curSet.replace(/search-container/, "search-container,searchwp-highlight-container");
+        }
+        else { // at the end
+          set = nav.currentSet + ",searchwp-highlight-container";
+        }
+        nav.setAttribute("currentset", set);
+        nav.currentSet = set;
+        document.persist("nav-bar", "currentset");
+        // If you don't do the following call, funny things happen
+        try {
+          BrowserToolboxCustomizeDone(true);
+        }
+        catch (e) { }
+      }
+    }
+    catch(e) { }
+
+    gSearchWP.Preferences.firstLaunch = false;
+  }
+
+  /**
+   * Called when the customization of the toolbar is finised.
+   * @return the original method.
+   */
+  this.onCustomizeDone = function() {
+    gSearchWP.Preferences.highlighted = false;
+    return _oldCustomizeDone();
+  }
+
+  /**
+   * Progress Listener to automatically highlight terms on page load.
+   */
+  this.progressListener = new function() {
+    this.QueryInterface = function(aIID) {
+      if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+        aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+        aIID.equals(Components.interfaces.nsISupports)) {
+        return this;
+      }
+      throw Components.results.NS_NOINTERFACE;
+    }
+
+    this.onProgressChange = function (aWebProgress, aRequest, aCurSelfProgress,
+                                      aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress) {}
+
+    this.onStatusChange = function(aWebProgress, aRequest, aStatus, message) {}
+
+    this.onSecurityChange = function(aWebProgress, aRequest, aState) {}
+
+    this.onLinkIconAvailable = function(a) {}
+
+    this.onStateChange = function (aWebProgress, aRequest, aStateFlags, aStatus) {
+      if (aStateFlags & Components.interfaces.nsIWebProgressListener.STATE_STOP) {
+        /**
+         * XXXLegege (July 15th, 2005): Some users report that the page never stop
+         * to load. The solution is to make the highlighting asynchrone.
+         */
+        setTimeout(function() {
+          gSearchWP.Highlighting.refresh();
+        }, 0);
+      }
+    }
+
+    this.onLocationChange = function(aProgress, aRequest, aLocation) {}
+  }
+
+  /**
+   * Preferences Observer
+   */
+  this.preferencesObserver = new function() {
+    var branch = gSearchWP.Preferences.branch;
+
+    this.register = function() {
+      branch.addObserver("", this, false);
+    }
+
+    this.unregister = function() {
+      if (!branch) return;
+      branch.removeObserver("", this, false);
+    }
+
+    this.observe = function(aSubject, aTopic, aData) {
+      if (aTopic != "nsPref:changed") {
+        return;
+      }
+
+      switch (aData) {
+        case gSearchWP.Preferences.PREF_HIGHLIGHT_STATE:
+          setTimeout(function() {
+            var item = gSearchWP.Highlighting.highlightButton;
+            if (item) {
+              item.setAttribute("checked", gSearchWP.Preferences.highlighted);
+            }
+            gSearchWP.Highlighting.refresh();
+          }, 0);
+          break;
+        case gSearchWP.Preferences.PREF_HIGHLIGHT_MATCH_CASE:
+          setTimeout(function() {
+            var item = gSearchWP.Highlighting.highlightButton;
+            if (item) {
+              item.setAttribute("matchcase", gSearchWP.Preferences.highlightMatchCase);
+            }
+            gSearchWP.Highlighting.refresh();
+          }, 0);
+          break;
+        case gSearchWP.Preferences.PREF_HIGHLIGHT_MINLENGTH:
+          setTimeout(function() {
+            gSearchWP.Highlighting.refresh();
+          }, 0);
+          break;
+      }
+    }
+  }
+}
+
+addEventListener("load", function(aEvent) { gSearchWP.Overlay.onLoad(aEvent); }, false);
---
+++ tags/searchwp-2.4b1/src/chrome/content/tokenizer.js
@@ -0,0 +1,166 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is SearchWP.
+ *
+ * The Initial Developer of the Original Code is 
+ *  Georges-Etienne Legendre <legege@legege.com> <http://legege.com>.
+ * Portions created by the Initial Developer are Copyright (C) 2004-2008.
+ * All Rights Reserved.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+gSearchWP.Tokenizer = new function() {
+
+  /**
+   * @param aSearchString The search string.
+   * @return an array that contains, for each words, the label to display and
+   *   the className when the term is highlighted.
+   */
+  this.createTokensArray = function(aSearchString) {
+    return parseTerms(aSearchString);
+  }
+
+  /**
+   * @return true if the 2 tokens array are identicals
+   */
+  this.compare = function(aTokensArray1, aTokensArray2) {
+    if (aTokensArray1 && aTokensArray2) {
+      if (aTokensArray1.length == aTokensArray2.length) {
+        var i = 0;
+        while(i < aTokensArray2.length && aTokensArray2[i] == aTokensArray1[i]) {
+          i++;
+        }
+        if (i == aTokensArray2.length) { //Identical
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * From the Googlebar project: googlebarUtil.js
+   *
+   * @param aCriteria The search string.
+   * @return an array of terms.
+   */
+  function parseTerms(aCriteria) {
+    // quotes only matter when preceded by a space or a quote.
+    var terms = new Array();
+    var inQuote = false;
+    var haveTerm = false;
+
+    var currPtr  = 0; // the start of the current term
+    var prevChar = ' '; // the char we last saw
+
+    var val;
+
+    aCriteria = aCriteria.replace(/(allinanchor|allintext|allintitle|allinurl|author|bphonebook|cache|define|ext|filetype|group|id|inanchor|info|insubject|intext|intitle|inurl|link|location|movie|msgid|phonebook|related|rphonebook|safesearch|site|source|stocks|store|weather):/g, "");
+
+    for (var index = 0; index < aCriteria.length; index++) {
+      var currChar = aCriteria.charAt(index);
+
+      switch (currChar) {
+        case '+':
+        case '(':
+        case ')':
+        case '~':
+        case ',':
+        case ' ':
+        // double-byte space
+        case decodeURIComponent('%E3%80%80'):
+          // these characters do not occur on the search term buttons,
+          // except in quoted phrases
+          if (!inQuote && !haveTerm) {
+            currPtr = index;
+          }
+          else if (!inQuote && haveTerm) {
+            val = trimString(aCriteria.substring(currPtr, index));
+
+            if (val[0] != '-' && val != 'OR' && val != 'AND' && !inArray(terms, val)) {
+              terms.push(val);
+              currPtr = index;
+            }
+            haveTerm = false;
+          }
+          else if (inQuote && !haveTerm) {
+            haveTerm = true;
+            currPtr = index;
+          }
+          break;
+
+        case '"':
+          if (haveTerm) {
+            val = aCriteria.substring(currPtr, index);
+            if (!inArray(terms, val)) {
+              terms.push(val);
+            }
+          }
+
+          // phrases (enclosed in "") should result in only one button
+          if (!inQuote) {
+            inQuote = true;
+          }
+          else if (inQuote && haveTerm) {
+            inQuote = false;
+          }
+
+          haveTerm = false;
+          break;
+
+        default:
+          if (!haveTerm) {
+            haveTerm = true;
+            currPtr = index;
+          }
+      }
+      prevChar = currChar;
+    }
+
+    val = trimString(aCriteria.substring(currPtr, index));
+
+    if (haveTerm && val[0] != '-' && val != 'OR' && val != 'AND' && !inArray(terms, val)) {
+      terms.push(val);
+    }
+
+    return terms;
+  }
+
+  /**
+   * Determines if a given string is contained in an array.
+   * @param aArr The array.
+   * @param aStr The string.
+   * @return true if the string is contained in the array, false if not.
+   */
+  function inArray(aArr, aStr) {
+    for (var i = 0; i < aArr.length; i++) {
+      if (aArr[i].toLowerCase() == trimString(aStr.toLowerCase())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Trim a string.
+   * @param aString the string.
+   * @return the modified string.
+   */
+  function trimString(aString) {
+    if (!aString) {
+      return "";
+    }
+    return aString.replace(/(^\s+)|(\s+$)/g, '');
+  }
+}
