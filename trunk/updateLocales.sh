2783330a500bc9105fc8d76f586dd1154d0bfb50
Robert.Katic@gmail.com <Robert.Katic@gmail.com@76b47386-e832-0410-97a5-4fc110babfa0>
2011-11-28 21:46:13 +0000

    No duplicated locales.
    - Remove locale/src
    - Move locale/xpi to locale
    - Removed build.xpi::xpi-babelzilla
    - updateLocales.sh only `snv add` all locale files

--- trunk/updateLocales.sh
+++ trunk/updateLocales.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-echo "Copying locale files..."
-LOCALES_SRC="$1"
-LOCALES_DEST="$(dirname $0)/src/chrome/locale/xpi"
+echo "Adding locale files..."
+LOCALES="$(dirname $0)/src/chrome/locale"
 
-echo "Destination: $LOCALES_DEST"
-for locale in `ls $LOCALES_SRC`; do
-  echo "Copying file for $locale..."
-  mkdir -p $LOCALES_DEST/$locale/
-  cp -r $LOCALES_SRC/$locale/* $LOCALES_DEST/$locale/
-  svn add $LOCALES_DEST/$locale 2> /dev/null
-  svn add $LOCALES_DEST/$locale/* 2> /dev/null
+for locale in `ls $LOCALES`; do
+  echo "Adding files for $locale..."
+  svn add $LOCALES/$locale 2> /dev/null
+  svn add $LOCALES/$locale/* 2> /dev/null
 done
